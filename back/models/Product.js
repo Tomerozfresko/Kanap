fc2f25bc16e2bd72f3c8d9312f05857625e83949
Tomer Oz Fresko <ozfresko@gmail.com>
2022-09-14 10:15:30 +0200

    Price change for test purposes

--- back/models/Product.js
+++ back/models/Product.js
@@ -3,7 +3,7 @@ const products = [
     "colors": ["Blue", "White", "Black"],
     "_id": "107fb5b75607497b96722bda5b504926",
     "name": "Kanap Sinopé",
-    "price": 1849,
+    "price": 2000,
     "imageUrl": "kanap01.jpeg",
     "description": "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     "altTxt": "Photo of a blue sofa, two seats"
@@ -12,7 +12,7 @@ const products = [
     "colors": ["Black/Yellow", "Black/Red"],
     "_id": "415b7cacb65d43b2b5c1ff70f3393ad1",
     "name": "Kanap Cyllène",
-    "price": 4499,
+    "price": 4000,
     "imageUrl": "kanap02.jpeg",
     "description": "Morbi nec erat aliquam, sagittis urna non, laoreet justo. Etiam sit amet interdum diam, at accumsan lectus.",
     "altTxt": "Photo of a yellow and black sofa, four seats"
@@ -21,7 +21,7 @@ const products = [
     "colors": ["Green", "Red", "Orange"],
     "_id": "055743915a544fde83cfdfc904935ee7",
     "name": "Kanap Calycé",
-    "price": 3199,
+    "price": 3000,
     "imageUrl": "kanap03.jpeg",
     "description": "Pellentesque fermentum arcu venenatis ex sagittis accumsan. Vivamus lacinia fermentum tortor.Mauris imperdiet tellus ante.",
     "altTxt": "Photo of a green angle sofa, three seats"
@@ -30,7 +30,7 @@ const products = [
     "colors": ["Pink", "White"],
     "_id": "a557292fe5814ea2b15c6ef4bd73ed83",
     "name": "Kanap Autonoé",
-    "price": 1499,
+    "price": 1000,
     "imageUrl": "kanap04.jpeg",
     "description": "Donec mattis nisl tortor, nec blandit sapien fermentum at. Proin hendrerit efficitur fringilla. Lorem ipsum dolor sit amet.",
     "altTxt": "Photo of a pink sofa, one to two seats"
@@ -39,7 +39,7 @@ const products = [
     "colors": ["Grey", "Purple", "Blue"],
     "_id": "8906dfda133f4c20a9d0e34f18adcf06",
     "name": "Kanap Eurydomé",
-    "price": 2249,
+    "price": 2000,
     "imageUrl": "kanap05.jpeg",
     "description": "Ut laoreet vulputate neque in commodo. Suspendisse maximus quis erat in sagittis. Donec hendrerit purus at congue aliquam.",
     "altTxt": "Photo of a gray sofa, three seats"
@@ -48,7 +48,7 @@ const products = [
     "colors": ["Grey", "Navy"],
     "_id": "77711f0e466b4ddf953f677d30b0efc9",
     "name": "Kanap Hélicé",
-    "price": 999,
+    "price": 1000,
     "imageUrl": "kanap06.jpeg",
     "description": "Curabitur vel augue sit amet arcu aliquet interdum. Integer vel quam mi. Morbi nec vehicula mi, sit amet vestibulum.",
     "altTxt": "Photo of a gray sofa, two seats"
@@ -57,7 +57,7 @@ const products = [
     "colors": ["Red", "Silver"],
     "_id": "034707184e8e4eefb46400b5a3774b5f",
     "name": "Kanap Thyoné",
-    "price": 1999,
+    "price": 2000,
     "imageUrl": "kanap07.jpeg",
     "description": "EMauris imperdiet tellus ante, sit amet pretium turpis molestie eu. Vestibulum et egestas eros. Vestibulum non lacus orci.",
     "altTxt": "Photo of a red sofa, two seats"
@@ -66,7 +66,7 @@ const products = [
     "colors": ["Pink", "Brown", "Yellow", "White"],
     "_id": "a6ec5b49bd164d7fbe10f37b6363f9fb",
     "name": "Kanap orthosie",
-    "price": 3999,
+    "price": 4000,
     "imageUrl": "kanap08.jpeg",
     "description": "Mauris molestie laoreet finibus. Aenean scelerisque convallis lacus at dapibus. Morbi imperdiet enim metus rhoncus.",
     "altTxt": "Photo of a pink sofa, three seats"
